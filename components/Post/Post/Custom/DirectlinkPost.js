b1dcc9c32625bff8b1165629a414a8c0be066ca2
enotowitch4 <en.enotowitch4@gmail.com>
2025-07-02 12:11:31 +0300

    MF: DirectlinkPost: +facebookPixelId link
    - Added Facebook pixel link display conditionally based on the presence of facebookPixelId.
    - Updated styling for the safe page link to use a different color for better visibility.
    - Refactored props destructuring to include facebookPixelId for improved data handling.
    
    1 file modified.

--- components/Post/Post/Custom/DirectlinkPost.js
+++ components/Post/Post/Custom/DirectlinkPost.js
@@ -1,11 +1,17 @@
 import DirectlinkLandingpagePost from "./DirectlinkLandingpagePost";
 import Link from "next/link";
-import { BadgeMinus, BadgeDollarSign, Shield } from "lucide-react";
+import { BadgeMinus, BadgeDollarSign, Shield, Facebook } from "lucide-react";
 import { useTranslation } from "../../../Context/TranslationContext";
 
 export default function DirectlinkPost(props) {
-  const { name, destinationUrl, freeUrl, safePageUrl, shieldProtection } =
-    props.post;
+  const {
+    name,
+    destinationUrl,
+    freeUrl,
+    safePageUrl,
+    shieldProtection,
+    facebookPixelId,
+  } = props.post;
   const { t } = useTranslation();
 
   return (
@@ -56,13 +62,27 @@ export default function DirectlinkPost(props) {
                 href={safePageUrl}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="flex items-center text-sm text-blue-600 hover:!bg-accent/30 transition-colors"
+                className="flex items-center text-sm text-indigo-600 hover:!bg-accent/30 transition-colors"
               >
                 <Shield size={20} className="mr-1" />
                 {t("safePage")}:{" "}
                 {safePageUrl.length > 40
                   ? `${safePageUrl.substring(0, 40)}...`
                   : safePageUrl}
+              </Link>
+            </div>
+          )}
+
+          {facebookPixelId && (
+            <div className="f aic island">
+              <Link
+                href={`https://www.facebook.com/ads/library/?active_status=active&ad_type=all&country=US&view_all_page_id=100000000000000&search_type=keyword_unordered`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center text-sm text-blue-600 hover:!bg-accent/30 transition-colors"
+              >
+                <Facebook size={20} className="mr-1" />
+                {facebookPixelId}
               </Link>
             </div>
           )}
